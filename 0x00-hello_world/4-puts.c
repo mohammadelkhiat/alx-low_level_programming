cc94f22557190c9767ceda65c2af9cb2fc2ce01b
Mohammad <mohammadelkhiat@gmail.com>
2023-06-15 06:35:17 -0700

    0_0

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - using put
+ *
+ * description - A code that print with put function
+ *
+ * return - Always 0 (Succss)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
