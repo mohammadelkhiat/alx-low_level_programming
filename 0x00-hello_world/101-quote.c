48a76fabeeec16a7a36776bf4899e9f8706f757c
Mohammad <mohammadelkhiat@gmail.com>
2023-06-15 08:19:58 -0700

    0_0

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * main - prining
+ *
+ * description: printing a quote
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+
+	write(1, quo, 59);
+	return (1);
