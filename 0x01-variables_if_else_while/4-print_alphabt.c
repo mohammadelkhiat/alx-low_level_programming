079bfbb34e277c16a3ee7dbea8ba93f66547b40e
Mohammad <mohammadelkhiat@gmail.com>
2023-06-16 15:07:42 -0700

    0_0

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print !e,q
+ *
+ * description: printing alphapet without 'e' or 'q'
+ *
+ * Return: always 0
+*/
+
+int main(void)
+
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch == 'e' || ch == 'q')
+			ch++;
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
