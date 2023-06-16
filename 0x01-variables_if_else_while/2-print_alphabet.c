e81ddc10f90abac96edefce1fe341bf5a833d6b3
Mohammad <mohammadelkhiat@gmail.com>
2023-06-16 14:42:32 -0700

    0_0

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Alphapet
+ *
+ * description: Printing alphapet with putchar
+ *
+ * Return: Always 1 (success)
+*/
+
+int main(void)
+{
+	char ch = a
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar(\n)
+	return (0);
+}
