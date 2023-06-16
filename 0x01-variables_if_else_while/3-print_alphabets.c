26b88f4d7d3afdee2c5b38253defd0ea812b299a
Mohammad <mohammadelkhiat@gmail.com>
2023-06-16 14:57:56 -0700

    0_0

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - printing both
+ *
+ * description: in lower and upper case
+ *
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	char ch = 'a';
+	char CH = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+	return (0);
+}
