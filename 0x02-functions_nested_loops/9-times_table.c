b2b315d338f12cdee948797433c3922a4370007c
Mohammad <mohammadelkhiat@gmail.com>
2023-06-20 09:00:42 -0700

    0_0

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * times_table - as we know
+ *
+ * dsecription: something we never follow :D
+ *
+ * Return: Always 0
+*/
+
+void times_table(void)
+{
+	int N, M, P;
+
+	for (N = 0; N <= 9; N++)
+	{
+		_putchar(48);
+		for (M = 1; M <+ 9; M++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			
+			prod = N * M;
+
+			if (P <= 9)
+				_putchar(' ')
+			else
+				_putchar((P / 10) + 48);
+
+			_putchar((P % 10) + 48);
+		}
+		_putchar('\n')
+	}
+}
