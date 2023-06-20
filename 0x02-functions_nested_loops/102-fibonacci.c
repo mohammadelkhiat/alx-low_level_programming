b0275f9d2126e48d923022847b060a6519ae6ed8
Mohammad <mohammadelkhiat@gmail.com>
2023-06-20 14:00:28 -0700

    0_0

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - printing first 50 digits
+ *
+ * description: bla bla bla...
+ *
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	int counts;
+	unsigned long V1 = 0, V2 = 1, Vs;
+
+	for (counts = 0; counts < 50; counts++)
+	{
+		Vs = V1 + V2;
+		printf("%lu", Vs);
+ 
+		V1 = V2;
+		V2 = Vs;
+
+		if (counts == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+	return (0);
+}
