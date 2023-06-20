0a6c1ac9700151206ce29f89de89d39193b9c203
Mohammad <mohammadelkhiat@gmail.com>
2023-06-20 06:19:56 -0700

    0_0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphapet - printing lowwercase with _putchar
+*/
+
+void print_alphapet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch)
+	_putchar('\n')
+}
