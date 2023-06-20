718ce521acd9f14742600c6444f23a93a82b438c
Mohammad <mohammadelkhiat@gmail.com>
2023-06-20 09:29:09 -0700

    0_0

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,19 +10,10 @@ void print_to_98(int n)
 	int count;
 
 	if (n > 98)
-	{
 		for (count = n; count > 98; count--)
-			_putchar(count + 48);
-			_putchar(',');
-			_putchar(' ');
-	}
+			printf("%d, ", count);
 	else
-	{
 		for (count = n; count < 98; count++)
-			_putchar(count + 48);
-			_putchar(',');
-			_putchar(' ');
-	}
-	_putchar(98 + 48);
-	_putchar('\n');
+			printf("%d, ", count);
+	printf("98\n");
 }
