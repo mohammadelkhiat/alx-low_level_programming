6e8c0a494ba291e819d03f1fbaef1d8df7ec3fa5
Mohammad <mohammadelkhiat@gmail.com>
2023-06-20 06:12:03 -0700

    0_0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - printing
+ *
+ * Description: Print _putchar without stdio.h
+ *
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+		_putchar(str[ch]);
+	-putchar('\n');
+return (0);
+}
