4200aeca675c5c8887bc8f5c3e164f0227c7e38d
Mohammad <mohammadelkhiat@gmail.com>
2023-07-10 16:48:06 -0700

    0_0

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,10 +22,10 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 
 	for (x = 0; x < height; x++)
-	{		
+	{
 		mee[x] = malloc(sizeof(int) * width);
 
-		if (mee[x] == NULL)		
+		if (mee[x] == NULL)
 		{
 			for (; x >= 0; x--)
 			free(mee[x]);
