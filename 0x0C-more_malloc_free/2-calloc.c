8fd13ec0b0ea31751553974bc1670e89c1f1666e
Mohammad <mohammadelkhiat@gmail.com>
2023-07-14 17:24:29 -0700

    0_0

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,48 +1,46 @@
-#include <stdlib.h>
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * _memset - fills memory with a constant byte
- * @s: memory area to be filled
- * @b: char to copy
- * @n: number of times to copy b
+ * _memset -  initializes n bytes of memory to x
+ * @ptr: initial adress
+ * @x: variable to initialize with
+ * @n: number of bytes to initialize
  *
- * Return: pointer to the memory area s
+ * Return: Return pointer char (so movements are 1 byte)
 */
 
-char *_memset(char *s, char b, unsigned int n)
+char *_memset(char *PR, int s, unsigned int n)
 {
-	unsigned int i:
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
-	{
-		s[i] = b;
-	}
+		PR[i] = s;
 
-	return (s);
+	return (PR);
 }
 
 /**
- * *_calloc - allocates memory for an array
- * @nmemb: number of elements in an array
- * @size: size of each element
+ * _calloc -  allocates memory using malloc and initializes in 0
+ * @nmemb: number of elements of array to allocate
+ * @size: size of elements
  *
- * Return: pointer to allocated memory
-*/
-
+ * Return: Pointer to allocated memory or normal process termination
+ * with a status value of 98
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *ptr;
+	void *ptr;
+
 
 	if (nmemb == 0 || size == 0)
-		return (NULL);
+		return (0);
 
 	ptr = malloc(nmemb * size);
-
-	if (ptr == NULL)
-		return (NULL);
-
-	_memset(ptr, 0, nmemb * size);
+	if (ptr == 0)
+		return (0);
+	_memset(ptr, 0, size * nmemb);
 
 	return (ptr);
 }
