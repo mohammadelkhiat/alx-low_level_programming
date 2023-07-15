9caf3993524284bf9c6a829a223f137f8cae3064
Mohammad <mohammadelkhiat@gmail.com>
2023-07-14 17:26:02 -0700

    0_0

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -4,8 +4,8 @@
 
 /**
  * _memset -  initializes n bytes of memory to x
- * @ptr: initial adress
- * @x: variable to initialize with
+ * @PR: initial adress
+ * @s: variable to initialize with
  * @n: number of bytes to initialize
  *
  * Return: Return pointer char (so movements are 1 byte)
