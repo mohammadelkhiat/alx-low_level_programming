ca8faa48a2e448413b287845d083563bcd932a37
Mohammad <mohammadelkhiat@gmail.com>
2023-08-03 12:39:19 +0300

    task 3 file updeted with betty error fixed

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -5,7 +5,7 @@
  * @index: is the index, starting from 0 of the bit you want to set
  * @n: pointer to the number to change
  *
- * Returns: 1 if it worked, or -1 if an error occurred
+ * Return: 1 if it worked, or -1 if an error occurred
 */
 
 int set_bit(unsigned long int *n, unsigned int index)
