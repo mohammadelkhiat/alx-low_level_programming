a2a60c1664da57e0966c23532906c1d8bb861b24
Mohammad <mohammadelkhiat@gmail.com>
2023-08-03 11:37:39 +0300

    task 0 file updated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,24 +12,18 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int RTRN = 0;
+	int i;
+	unsigned int R = 0;
 
 	if (b == NULL)
 		return (0);
-	for (int i = 0; b[i] != '\0'; i++)
+
+	for (i = 0; b[i]; i++)
 	{
-		if (b[i] == '0')
-		{
-			RTRN = RTRN * 2;
-		}
-		else if (b[i] = '1')
-		{
-			RTRN = RTRN * 2 + 1;
-		}
-		else
-		{
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		}
+		R = R * 2 + (b[i] - '0');
 	}
-	return (RTRN);
+
+	return (R);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,12 +4,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-unsigned int binary_to_uint(const char *b);
-void print_binary(unsigned long int n);
-int get_bit(unsigned long int n, unsigned int index);
-int set_bit(unsigned long int *n, unsigned int index);
-int clear_bit(unsigned long int *n, unsigned int index);
-unsigned int flip_bits(unsigned long int n, unsigned long int m);
-int get_endianness(void);
+unsigned int binary_to_uint(const char *b); /*task 0*/
+void print_binary(unsigned long int n); /*task 1*/
+int get_bit(unsigned long int n, unsigned int index); /*task 2*/
+int set_bit(unsigned long int *n, unsigned int index); /*task 3*/
+int clear_bit(unsigned long int *n, unsigned int index); /*task 4*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m); /*task 5*/
+int _atoi(const char *s);
+int _putchar(char c);
+int get_endianness(void); /*task 6*/
 
 #endif
