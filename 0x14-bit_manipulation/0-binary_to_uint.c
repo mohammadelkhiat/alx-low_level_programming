2feafc3b6a680fc520af82604fa678416d730710
Mohammad <mohammadelkhiat@gmail.com>
2023-10-26 23:52:00 +0300

    0_0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * binary to unit - main functuions
+ * 
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+    if (*b = NULL)
+    {
+        return (0);
+    }
+       
