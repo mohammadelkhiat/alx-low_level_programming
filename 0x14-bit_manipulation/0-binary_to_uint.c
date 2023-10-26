de0d61c4ba4bffd453e11772bbaf76f15192e504
Mohammad <mohammadelkhiat@gmail.com>
2023-10-26 23:53:42 +0200

    0_0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,25 +1,27 @@
 #include "main.h"
 
 /**
- * binary to unit - main functuions
+ * binary_to_uint - main functuions
+ * @b: pointer to a string
+ * Return: 0 or value of dec
 */
 
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int dec = 0;
-    int i;
+	unsigned int dec = 0;
+	int i;
 
-    if (b == NULL)
-    {
-        return (0);
-    }
+	if (b == NULL)
+	{
+		return (0);
+	}
 
-    for (i = 0; b[i] != '\0'; i++)
-    {
-        if (b[i] != '0' && b[i] != '1')
-            return (0);
-    dec = ((dec << 1) | (b[i] - '0'));
-    }
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+		return (0);
+	dec = ((dec << 1) | (b[i] - '0'));
+	}
 
-    return (dec);
+	return (dec);
 }
