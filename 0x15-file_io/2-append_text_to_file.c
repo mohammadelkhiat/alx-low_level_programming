402e4b861280853e523774335448da28a233a034
Mohammad <mohammadelkhiat@gmail.com>
2023-10-31 02:31:57 +0200

    0_0

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * create_file - reads a text
+ * @filename: file name
+ * @text_content: text_content
+ * Return: letter
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int FOpen;
+	ssize_t byteSize;
+
+	FOpen = open(filename, O_WRONLY | O_APPEND );
+
+	if (FOpen == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		byteSize = write(FOpen, text_content, strlen(text_content));
+		if (byteSize == -1)
+		{
+			close(FOpen);
+			return (-1);
+		}
+		close(FOpen);
+	}
+	close(FOpen);
+	return (1);
+}
