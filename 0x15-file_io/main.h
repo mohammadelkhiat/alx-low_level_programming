7082eba58250e9539c6269bcb47d34bbe50f13f3
Mohammad <mohammadelkhiat@gmail.com>
2023-10-31 02:04:35 +0200

    0_0

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,21 +10,5 @@
 #include <stdint.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
-int create_file(const char *filename, char *text_content);
-int append_text_to_file(const char *filename, char *text_content);
-char *create_buffer(char *file);
-void close_file(int fd);
-
-typedef struct
-{
-        uint8_t magic[ELF_MAGIC_SIZE];
-        uint8_t class;
-        uint8_t data;
-        uint8_t version;
-        uint8_t os_abi;
-        uint8_t abi_version;
-        uint16_t type;
-        uint64_t entry;
-} ElfHeader;
 
 #endif
